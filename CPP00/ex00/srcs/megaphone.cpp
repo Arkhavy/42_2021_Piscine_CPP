3de837f52ee300f4022f430dcacb0c953c57ceeb
Arkhavy | ljohnson <ljohnson@z4r10p5.42lyon.fr>
2022-07-10 16:52:15 +0200

    update ex00 + starting ex01

--- CPP00/ex00/srcs/megaphone.cpp
+++ CPP00/ex00/srcs/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 08:30:25 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/07/10 11:29:07 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/07/10 16:51:51 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 int	main(int ac, char **av)
 {
 	std::string	str;
-	std::locale	loc;
 
 	if (ac < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -25,7 +24,7 @@ int	main(int ac, char **av)
 		for (int i = 1; i < ac; i++)
 			str += av[i];
 		for (int i = 0; i < (int)str.size(); i++)
-			str[i] = std::toupper(str[i], loc);
+			str[i] = std::toupper(str[i]);
 		std::cout << str << std::endl;
 	}
 	return (0);
