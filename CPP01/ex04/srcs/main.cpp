0b462fe536013db8be2b17d1403b9e3818b8482c
Arkhavy | ljohnson <ljohnson@z4r10p5.42lyon.fr>
2022-07-17 11:10:19 +0200

    swap is working, need to write on ofstream now

--- CPP01/ex04/srcs/main.cpp
+++ CPP01/ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:00:49 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/07/17 10:42:46 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/07/17 11:08:33 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,15 +43,26 @@ std::string	get_file_content(std::string filename)
 
 std::string	swap_content_parts(std::string content, std::string arg1, std::string arg2)
 {
-	
+	size_t	index = 0;
+
+	while (1)
+	{
+		index = content.find(arg1);
+		if (index == std::string::npos)
+			break ;
+		content.erase(index, arg1.size());
+		content.insert(index, arg2);
+	}
+	std::cout << content << std::endl;
+	return (content);
 }
 
-void	write_file_content(std::string filename, std::string content)
-{
-	std::ofstream	ofs(filename);
+// void	write_file_content(std::string filename, std::string content)
+// {
+// 	std::ofstream	ofs(filename);
 	
-	ofs.close();
-}
+// 	ofs.close();
+// }
 
 int	main(int ac, char **av)
 {
@@ -72,6 +83,6 @@ int	main(int ac, char **av)
 	content = get_file_content(filename);
 	content = swap_content_parts(content, arg1, arg2);
 	filename.append(".replace");
-	write_file_content(filename, content);
+	// write_file_content(filename, content);
 	return (0);
 }
