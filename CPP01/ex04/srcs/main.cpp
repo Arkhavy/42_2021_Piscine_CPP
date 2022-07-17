d49b63a8ed214beb09a7fc8e37d476c5fe5abb4c
Arkhavy | ljohnson <ljohnson@z4r10p5.42lyon.fr>
2022-07-17 11:27:58 +0200

    check error should be done, will check later if everything is good

--- CPP01/ex04/srcs/main.cpp
+++ CPP01/ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:00:49 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/07/17 11:08:33 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/07/17 11:27:13 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 #include <string>
 #include <fstream>
 
-void	print_error(std::string message)
+int	print_error(std::string message)
 {
 	std::cout << "\033[31m" << "\033[1m";
 	std::cout << message;
 	std::cout << "\033[0m" << std::endl;
+	return (1);
 }
 
 std::string	get_file_content(std::string filename)
@@ -41,48 +42,67 @@ std::string	get_file_content(std::string filename)
 	return (file_content);
 }
 
-std::string	swap_content_parts(std::string content, std::string arg1, std::string arg2)
+std::string	swap_content_parts(std::string content, std::string s1, std::string s2)
 {
 	size_t	index = 0;
 
-	while (1)
+	while (42)
 	{
-		index = content.find(arg1);
+		index = content.find(s1);
 		if (index == std::string::npos)
 			break ;
-		content.erase(index, arg1.size());
-		content.insert(index, arg2);
+		content.erase(index, s1.size());
+		content.insert(index, s2);
 	}
-	std::cout << content << std::endl;
 	return (content);
 }
 
-// void	write_file_content(std::string filename, std::string content)
-// {
-// 	std::ofstream	ofs(filename);
+void	write_file_content(std::string filename, std::string content)
+{
+	std::ofstream	ofs(filename);
 	
-// 	ofs.close();
-// }
+	ofs << content;
+	ofs.close();
+}
+
+int	check_user_input(char **av)
+{
+	std::string		s1;
+	std::string		s2;
+	
+	if (!av[0] || !av[1] || !av[2] || !av[0][0] || !av[1][0] || !av[2][0])
+		return (print_error("Invalid argument somewhere in user input, try again !"));
+	std::ifstream	ifs(av[0]);
+	if (!ifs.good())
+		return (print_error("file does not exist or can't be opened."));
+	s1 = av[1];
+	s2 = av[2];
+	if (s1 == s2)
+		return (print_error("both s1 and s2 are identical, please change one !"));
+	ifs.close();
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
 	std::string		filename;
-	std::string		arg1;
-	std::string		arg2;
+	std::string		s1;
+	std::string		s2;
 	std::string		content;
 
 	if (ac != 4)
 	{
 		print_error("Invalid number of Arguments");
-		print_error("Usage : ./replace <file> <str1> <str2>");
-		return (1);
+		return (print_error("Usage : ./replace <file> <str1> <str2>"));
 	}
+	if (check_user_input(&av[1]))
+		return (1);
 	filename = av[1];
-	arg1 = av[2];
-	arg2 = av[3];
+	s1 = av[2];
+	s2 = av[3];
 	content = get_file_content(filename);
-	content = swap_content_parts(content, arg1, arg2);
+	content = swap_content_parts(content, s1, s2);
 	filename.append(".replace");
-	// write_file_content(filename, content);
+	write_file_content(filename, content);
 	return (0);
 }
