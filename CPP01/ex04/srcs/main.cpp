52f485415d7c1f06bf1b933899b124aa871705ac
arkstrix <arkhavy@gmail.com>
2022-11-10 16:15:18 +0100

    fixed ifs and ofs things with string and char* + added error case on ofs

--- CPP01/ex04/srcs/main.cpp
+++ CPP01/ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:00:49 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/07/17 12:29:43 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/11/10 16:11:26 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,20 @@
 int	print_error(std::string message)
 {
 	std::cout << "\033[31m" << "\033[1m";
+	std::cout << "[ERROR]: ";
 	std::cout << message;
 	std::cout << "\033[0m" << std::endl;
 	return (1);
 }
 
-std::string	get_file_content(std::string filename)
+std::string	get_file_content(const char* filename)
 {
-	std::ifstream	ifs(filename);
+	std::ifstream	ifs;
 	std::string		file_content;
 	int				fcontent_len;
 	char			*fcontent;
 
+	ifs.open(filename, std::ifstream::in);
 	ifs.seekg (0, ifs.end);
 	fcontent_len = ifs.tellg();
 	ifs.seekg (0, ifs.beg);
@@ -57,10 +59,16 @@ std::string	swap_content_parts(std::string content, std::string s1, std::string 
 	return (content);
 }
 
-void	write_file_content(std::string filename, std::string content)
+void	write_file_content(const char* filename, std::string content)
 {
-	std::ofstream	ofs(filename);
+	std::ofstream	ofs;
 	
+	ofs.open(filename, std::ofstream::out);
+	if (!ofs.good())
+	{
+		print_error("Outfile already exist and can't be opened.");
+		return ;
+	}
 	ofs << content;
 	ofs.close();
 }
@@ -74,7 +82,7 @@ int	check_user_input(char **av)
 		return (print_error("Invalid argument somewhere in user input, try again !"));
 	std::ifstream	ifs(av[0]);
 	if (!ifs.good())
-		return (print_error("file does not exist or can't be opened."));
+		return (print_error("Infile does not exist or can't be opened."));
 	s1 = av[1];
 	s2 = av[2];
 	if (s1 == s2)
@@ -85,10 +93,11 @@ int	check_user_input(char **av)
 
 int	main(int ac, char **av)
 {
-	std::string		filename;
-	std::string		s1;
-	std::string		s2;
-	std::string		content;
+	const char*	filename;
+	std::string	newfilename;
+	std::string	s1;
+	std::string	s2;
+	std::string	content;
 
 	if (ac != 4)
 	{
@@ -98,11 +107,13 @@ int	main(int ac, char **av)
 	if (check_user_input(&av[1]))
 		return (1);
 	filename = av[1];
+	newfilename = av[1];
 	s1 = av[2];
 	s2 = av[3];
 	content = get_file_content(filename);
 	content = swap_content_parts(content, s1, s2);
-	filename.append(".replace");
+	newfilename.append(".replace");
+	filename = newfilename.c_str();
 	write_file_content(filename, content);
 	return (0);
 }
