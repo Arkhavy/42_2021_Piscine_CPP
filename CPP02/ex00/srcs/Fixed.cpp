0e919026f15dedf1f924c7c2feb7c97f545d821c
arkstrix <arkhavy@gmail.com>
2022-11-15 09:57:30 +0100

    updated the display

--- CPP02/ex00/srcs/Fixed.cpp
+++ CPP02/ex00/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 08:45:28 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/07/22 13:29:18 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/11/15 09:41:49 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,27 @@
 /* ************************************************************************** */
 Fixed::Fixed()
 {
-	std::cout << "\033[2m" << "Fixed Default Constructor called." << "\033[0m" << std::endl;
+	std::cout << "\033[2m";
+	std::cout << "Fixed Default Constructor called.";
+	std::cout << "\033[0m" << std::endl;
 	this->value = 0;
 	return ;
 }
 
 Fixed::Fixed(Fixed const& src)
 {
-	std::cout << "\033[2m" << "Fixed Copy Constructor called." << "\033[0m" << std::endl;
+	std::cout << "\033[2m";
+	std::cout << "Fixed Copy Constructor called.";
+	std::cout << "\033[0m" << std::endl;
 	*this = src;
 	return ;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "\033[2m" << "Fixed Destructor called." << "\033[0m" << std::endl;
+	std::cout << "\033[2m";
+	std::cout << "Fixed Destructor called.";
+	std::cout << "\033[0m" << std::endl;
 	return ;
 }
 
@@ -40,7 +46,9 @@ Fixed::~Fixed()
 /* ************************************************************************** */
 Fixed&	Fixed::operator=(Fixed const& rhs)
 {
-	std::cout << "\033[2m" << "Fixed Copy assignment operator called." << "\033[0m" << std::endl;
+	std::cout << "\033[2m";
+	std::cout << "Fixed Copy assignment operator called.";
+	std::cout << "\033[0m" << std::endl;
 	this->value = rhs.getRawBits();
 	return (*this);
 }
@@ -50,13 +58,17 @@ Fixed&	Fixed::operator=(Fixed const& rhs)
 /* ************************************************************************** */
 int	Fixed::getRawBits() const
 {
-	std::cout << "\033[2m" << "Fixed getRawBits member function called" << "\033[0m" << std::endl;
+	std::cout << "\033[2m";
+	std::cout << "Fixed getRawBits member function called";
+	std::cout << "\033[0m" << std::endl;
 	return (this->value);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "\033[2m" << "Fixed setRawBits member function called" << "\033[0m" << std::endl;
+	std::cout << "\033[2m";
+	std::cout << "Fixed setRawBits member function called";
+	std::cout << "\033[0m" << std::endl;
 	this->value = raw;
 	return ;
 }
