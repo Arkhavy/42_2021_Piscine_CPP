df6813b3c5cf1d4fa5d08f273ea230f674e488e1
arkstrix <arkhavy@gmail.com>
2022-12-07 18:59:30 +0100

    FragTrap done, should be working fine

--- CPP03/ex02/srcs/FragTrap.cpp
+++ CPP03/ex02/srcs/FragTrap.cpp
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 12:16:58 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/12/07 18:31:24 by ljohnson         ###   ########lyon.fr   */
+/*   Created: 2022/12/07 18:30:27 by ljohnson          #+#    #+#             */
+/*   Updated: 2022/12/07 18:50:42 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ScavTrap.hpp>
+#include <FragTrap.hpp>
 
 #define YELLOW "\033[33m"
 #define CYAN "\033[36m"
@@ -22,47 +22,47 @@
 /* ************************************************************************** */
 /* Constructors & Destructors */
 /* ************************************************************************** */
-ScavTrap::ScavTrap()
+FragTrap::FragTrap()
 {
 	std::cout << FAINT;
-	this->name = "SC4V-TP";
+	this->name = "FR4G-TP";
 	this->hit_points = 100;
-	this->energy_points = 50;
-	this->attack_damage = 20;
+	this->energy_points = 100;
+	this->attack_damage = 30;
 	this->max_hit_points = this->hit_points;
-	this->max_energy_points = this->energy_points;
-	std::cout << "ScavTrap " << this->name << " default constructor called";
+	this->max_energy_points = this->max_energy_points;
+	std::cout << "Fragtrap " << this->name << " default constructor called";
 	std::cout << FWHITE << std::endl;
 	return ;
 }
 
-ScavTrap::ScavTrap(std::string name)
+FragTrap::FragTrap(std::string name)
 {
 	std::cout << FAINT;
 	this->name = name;
 	this->hit_points = 100;
-	this->energy_points = 50;
-	this->attack_damage = 20;
+	this->energy_points = 100;
+	this->attack_damage = 30;
 	this->max_hit_points = this->hit_points;
 	this->max_energy_points = this->energy_points;
-	std::cout << "ScavTrap " << this->name << " constructor called";
+	std::cout << "Fragtrap " << this->name << " default constructor called";
 	std::cout << FWHITE << std::endl;
 	return ;
 }
 
-ScavTrap::ScavTrap(ScavTrap const& src) : ClapTrap(src)
+FragTrap::FragTrap(FragTrap const& src) : ClapTrap(src)
 {
 	*this = src;
 	std::cout << FAINT;
-	std::cout << "ScavTrap " << this->name << " copy constructor called";
+	std::cout << "FragTrap " << this->name << " copy constructor called";
 	std::cout << FWHITE << std::endl;
 	return ;
 }
 
-ScavTrap::~ScavTrap()
+FragTrap::~FragTrap()
 {
 	std::cout << FAINT;
-	std::cout << "ScavTrap " << this->name << " destructor called";
+	std::cout << "FragTrap " << this->name << " destructor called";
 	std::cout << FWHITE << std::endl;
 	return ;
 }
@@ -70,49 +70,19 @@ ScavTrap::~ScavTrap()
 /* ************************************************************************** */
 /* Public member functions */
 /* ************************************************************************** */
-void	ScavTrap::attack(const std::string& target)
-{
-	if (this->hit_points == 0)
-	{
-		std::cout << YELLOW << BOLD;
-		std::cout << "A dead " << this->name << " cannot attack " << target;
-		std::cout << FWHITE << std::endl;
-		return ;
-	}
-	if (this->energy_points == 0)
-	{
-		std::cout << YELLOW << BOLD;
-		std::cout << "An exhausted " << this->name << " cannot attack " << target;
-		std::cout << FWHITE << std::endl;
-		return ;
-	}
-	this->energy_points--;
-	std::cout << "ScavTrap ";
-	std::cout << CYAN << BOLD;
-	std::cout << this->name << FWHITE;
-	std::cout << " attacks ";
-	std::cout << CYAN << BOLD;
-	std::cout << target << FWHITE;
-	std::cout << ", dealing ";
-	std::cout << YELLOW << BOLD;
-	std::cout << this->attack_damage << FWHITE;
-	std::cout << " damages !" << std::endl;
-	return ;
-}
-
-void	ScavTrap::guardGate()
+void	FragTrap::highFivesGuys(void)
 {
 	std::cout << PURPLE;
-	std::cout << "ScavTrap " << this->name << " entered Gate Keeper mode ! ";
+	std::cout << this->name << " IS ASKING FOR A HIGH FIVE !" << std::endl;
 	std::cout << FWHITE << FAINT;
-	std::cout << "Wtf is happening ...";
+	std::cout << "Don't do too much, Claptraps don't deserve that much attention.";
 	std::cout << FWHITE << std::endl;
+	
 }
-
 /* ************************************************************************** */
 /* Arithmetic Operator Overloads */
 /* ************************************************************************** */
-ScavTrap&	ScavTrap::operator=(ScavTrap const& rhs)
+FragTrap&	FragTrap::operator=(FragTrap const& rhs)
 {
 	this->name = rhs.name;
 	this->hit_points = rhs.hit_points;
--- CPP03/ex02/srcs/main.cpp
+++ CPP03/ex02/srcs/main.cpp
@@ -6,13 +6,51 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 13:03:16 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/12/07 18:25:27 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/12/07 18:55:38 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ScavTrap.hpp>
+#include <FragTrap.hpp>
 
 int	main(void)
 {
+	ClapTrap	cp1;
+	ScavTrap	sp1;
+	FragTrap	fp1;
+
+	std::cout << "--------------------" << std::endl;
+	
+	ClapTrap	cp2("ouaf");
+	ScavTrap	sp2("graou");
+	FragTrap	fp2("oskour");
+
+	std::cout << "--------------------" << std::endl;
+
+	ClapTrap	cp3(cp2);
+	ScavTrap	sp3(sp2);
+	FragTrap	fp3(fp2);
+	
+	std::cout << "--------------------" << std::endl;
+	cp3.attack("sbire");
+	cp3.beRepaired(2);
+	cp3.takeDamage(3);
+	cp3.beRepaired(2);
+
+	std::cout << std::endl;
+	sp3.attack("sbire");
+	sp3.beRepaired(2);
+	sp3.takeDamage(3);
+	sp3.beRepaired(2);
+	sp3.guardGate();
+
+	std::cout << std::endl;
+	fp3.attack("sbire");
+	fp3.beRepaired(2);
+	fp3.takeDamage(3);
+	fp3.beRepaired(2);
+	fp3.highFivesGuys();
+
+	std::cout << "--------------------" << std::endl;
 	return (0);
 }
