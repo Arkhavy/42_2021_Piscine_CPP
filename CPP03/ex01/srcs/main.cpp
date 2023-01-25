0301c4b2e04f298d92d627dcddb2a038ef59dfab
Arkhavy | ljohnson <ljohnson@z4r10p5.42lyon.fr>
2023-01-25 13:54:48 +0100

    little fix on ClapTrap max HP, naming in main and line orders in constructors

--- CPP03/ex01/srcs/main.cpp
+++ CPP03/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 13:03:16 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/12/07 13:29:35 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2023/01/25 13:39:46 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,28 @@ int	main(void)
 	
 	std::cout << "--------------------" << std::endl;
 
-	ClapTrap	cp2("ouaf");
-	ScavTrap	sp2("graou");
+	ClapTrap	cp2("Ouaf");
+	ScavTrap	sp2("Graou");
 
 	std::cout << "--------------------" << std::endl;
 
 	ClapTrap	cp3(cp2);
 	ScavTrap	sp3(sp2);
+
 	std::cout << "--------------------" << std::endl;
 
-	ClapTrap	cp4("sbire");
-	ScavTrap	sp4("guardian");
+	ClapTrap	cp4("Minion");
+	ScavTrap	sp4("Guardian");
 
-	cp4.attack("guardian");
+	std::cout << "--------------------" << std::endl;
+
+	cp4.attack("Guardian");
 	sp4.guardGate();
 	sp4.takeDamage(0); // OK -0HP
 	sp4.beRepaired(2); // Error Case HP max
 	sp4.takeDamage(1); // OK -1HP
 	sp4.beRepaired(2); // Semi Error Case beyond HP max
-	sp4.attack("sbire");
+	sp4.attack("Minion");
 	cp4.takeDamage(20); // One Shot lol
 
 	std::cout << "--------------------" << std::endl;
