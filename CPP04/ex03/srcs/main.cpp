444b0aa6827feefc814b52537aa031a489570020
Loïc Johnson <ljohnson@z2r4p6.42lyon.fr>
2023-02-13 16:53:19 +0100

    ex03: updated main with subject + cf previous commit to see todolist

---
+++ CPP04/ex03/srcs/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 16:51:00 by ljohnson          #+#    #+#             */
+/*   Updated: 2023/02/13 16:52:19 by ljohnson         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Character.hpp>
+#include <Cure.hpp>
+#include <Ice.hpp>
+#include <MateriaSource.hpp>
+
+int	main(void)
+{
+	IMateriaSource* src = new MateriaSource();
+
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
+	return (0);
+}
