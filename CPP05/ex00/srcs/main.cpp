7209b7e34c93b5aae82d289db133edc597fdd6e3
Loïc Johnson <ljohnson@z2r4p6.42lyon.fr>
2023-02-14 17:11:14 +0100

    CPP05 ex00: test main done, next ex inc

--- CPP05/ex00/srcs/main.cpp
+++ CPP05/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 14:54:51 by ljohnson          #+#    #+#             */
-/*   Updated: 2023/02/14 15:15:48 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2023/02/14 17:10:31 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,74 +19,45 @@ void	print_err(char const* what)
 	std::cerr << FWHITE << std::endl;
 }
 
+void	init_bureaucrat(std::string const& name, unsigned int const grade)
+{
+	Bureaucrat	worker(name, grade);
+
+	std::cout << worker << std::endl;
+
+	worker.increment_grade();
+	std::cout << worker << std::endl;
+	worker.decrement_grade();
+	std::cout << worker << std::endl;
+	worker.decrement_grade();
+	std::cout << worker << std::endl;
+}
+
 int	main(void)
 {
-	try {Bureaucrat	b_high("Emperor", 1);}
-	catch (Bureaucrat::GradeTooHighException& e) {print_err(e.what());}
-	catch (Bureaucrat::GradeTooLowException& e) {print_err(e.what());}
-
-	std::cout << std::endl;
-
-	try {Bureaucrat	b_low("Stormtrooper", 150);}
-	catch (Bureaucrat::GradeTooHighException& e) {print_err(e.what());}
-	catch (Bureaucrat::GradeTooLowException& e) {print_err(e.what());}
-
-	std::cout << std::endl;
-
-	try {Bureaucrat	b_middle("Jedi", 50);}
-	catch (Bureaucrat::GradeTooHighException& e) {print_err(e.what());}
-	catch (Bureaucrat::GradeTooLowException& e) {print_err(e.what());}
-
-	std::cout << std::endl;
-
-	try {Bureaucrat	b_err_high("Leia", 0);}
-	catch (Bureaucrat::GradeTooHighException& e) {print_err(e.what());}
-	catch (Bureaucrat::GradeTooLowException& e) {print_err(e.what());}
-
-	std::cout << std::endl;
-
-	try {Bureaucrat	b_err_low("JarJar", 151);}
-	catch (Bureaucrat::GradeTooHighException& e) {print_err(e.what());}
-	catch (Bureaucrat::GradeTooLowException& e) {print_err(e.what());}
-
-
-		// std::cout << b_high << std::endl;
-		// std::cout << b_low << std::endl;
-		// std::cout << b_middle << std::endl;
-	// catch (Bureaucrat::GradeTooHighException& e) {std::cerr << e.what() << std::endl;}
-	// catch (Bureaucrat::GradeTooLowException& e) {std::cerr << e.what() << std::endl;}
-
-
-	// std::cout << std::endl << "--------------------------------------------------" << std::endl;
-
-	// try
-	// {
-	// 	b_high.increment_grade();
-	// 	std::cout << b_high << std::endl;
-	// 	b_high.decrement_grade();
-	// 	std::cout << b_high << std::endl;
-	// }
-
-	// std::cout << std::endl << "--------------------------------------------------" << std::endl;
-
-	// try
-	// {
-	// 	b_low.decrement_grade();
-	// 	std::cout << b_low << std::endl;
-	// 	b_low.increment_grade();
-	// 	std::cout << b_low << std::endl;
-	// }
-
-	// std::cout << std::endl << "--------------------------------------------------" << std::endl;
-
-	// try
-	// {
-	// 	b_middle.increment_grade();
-	// 	std::cout << b_middle << std::endl;
-	// 	b_middle.decrement_grade();
-	// 	std::cout << b_middle << std::endl;
-	// }
-	// catch
-
+	for (int i = 0; i < 5; i++)
+	{
+		try
+		{
+			switch(i)
+			{
+				case 0:
+					init_bureaucrat("Emperor", 1); break ;
+				case 1:
+					init_bureaucrat("Stormtrooper", 150); break ;
+				case 2:
+					init_bureaucrat("Jedi", 50); break ;
+				case 3:
+					init_bureaucrat("Leia", 0); break ;
+				case 4:
+					init_bureaucrat("JarJar", 151); break ;
+				default:
+					print_err("Ouaf!");
+			}
+		}
+		catch (Bureaucrat::GradeTooHighException& e) {print_err(e.what());}
+		catch (Bureaucrat::GradeTooLowException& e) {print_err(e.what());}
+		std::cout << "--------------------------------------------------" << std::endl;
+	}
 	return (0);
 }
