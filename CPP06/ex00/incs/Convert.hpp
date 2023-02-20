993868e99c26a8ad682408c888e05c3cddbcb3d1
Loïc Johnson <ljohnson@z2r4p6.42lyon.fr>
2023-02-20 19:23:27 +0100

    CPP06 ex00: Added Convert.hpp/cpp files which contain different exceptions to throw depending on the error case of the conversion

---
+++ CPP06/ex00/incs/Convert.hpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 18:20:02 by ljohnson          #+#    #+#             */
+/*   Updated: 2023/02/20 18:54:09 by ljohnson         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include <climits>
+#include <cstdlib>
+
+#define YELLOW "\033[33m"
+#define CYAN "\033[36m"
+#define PURPLE "\033[35m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define BOLD "\033[1m"
+#define FAINT "\033[2m"
+#define FWHITE "\033[0m"
+
+/* ************************************************************************** */
+/* Char Exceptions */
+/* ************************************************************************** */
+class	CharImpossibleException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+class	CharNonDisplayableException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+/* ************************************************************************** */
+/* Int Exceptions */
+/* ************************************************************************** */
+class	IntOverflowException : public std::exception
+{
+	public:
+		virtual char const*	what() const throw();
+};
+
+class	IntImpossibleException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+/* ************************************************************************** */
+/* Float Exceptions */
+/* ************************************************************************** */
+class	FloatOverflowException : public std::exception
+{
+	public:
+		virtual char const*	what() const throw();
+};
+
+class	FloatInfNegativeException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+class	FloatInfPositiveException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+class	FloatNaNException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+/* ************************************************************************** */
+/* Double Exceptions */
+/* ************************************************************************** */
+class	DoubleOverflowException : public std::exception
+{
+	public:
+		virtual char const*	what() const throw();
+};
+
+class	DoubleInfNegativeException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+class	DoubleInfPositiveException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
+
+class	DoubleNaNException : public std::exception
+{
+	public:
+		virtual char const* what() const throw();
+};
---
+++ CPP06/ex00/srcs/Convert.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 18:24:38 by ljohnson          #+#    #+#             */
+/*   Updated: 2023/02/20 18:49:55 by ljohnson         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Convert.hpp>
+
+/* ************************************************************************** */
+/* Char Exceptions */
+/* ************************************************************************** */
+char const*	CharImpossibleException::what() const throw() {return ("char: Impossible");}
+char const*	CharNonDisplayableException::what() const throw() {return ("char: Non displayable");}
+
+/* ************************************************************************** */
+/* Int Exceptions */
+/* ************************************************************************** */
+char const*	IntOverflowException::what() const throw() {return ("int: Overflow");}
+char const*	IntImpossibleException::what() const throw() {return ("int: Impossible");}
+
+/* ************************************************************************** */
+/* Float Exceptions */
+/* ************************************************************************** */
+char const*	FloatOverflowException::what() const throw() {return ("float: Overflow");}
+char const*	FloatInfNegativeException::what() const throw() {return ("float: -inff");}
+char const*	FloatInfPositiveException::what() const throw() {return ("float: +inff");}
+char const*	FloatNaNException::what() const throw() {return ("float: NaN");}
+
+/* ************************************************************************** */
+/* Double Exceptions */
+/* ************************************************************************** */
+char const*	DoubleOverflowException::what() const throw() {return ("double: Overflow");}
+char const*	DoubleInfNegativeException::what() const throw() {return ("double: -inf");}
+char const*	DoubleInfPositiveException::what() const throw() {return ("double: +inf");}
+char const*	DoubleNaNException::what() const throw() {return ("double: NaN");}
+
+
