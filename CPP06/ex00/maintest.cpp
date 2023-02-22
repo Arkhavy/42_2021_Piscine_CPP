0840709878d1436460c408cd3028bf4449ccfeb7
arkstrix <arkhavy@gmail.com>
2023-02-22 21:35:00 +0100

    CPP06 ex00: maintest with overflow test, it works, need to add it to main exercise

--- CPP06/ex00/maintest.cpp
+++ CPP06/ex00/maintest.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 19:07:44 by ljohnson          #+#    #+#             */
-/*   Updated: 2023/02/22 19:30:20 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2023/02/22 21:34:15 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,11 @@ int	main(int ac, char** av)
 	if (ac != 2)
 		return (1);
 	std::string	str = av[1];
-	// long int	a = std::strtol(str.data(), NULL, 10);
+	double	a = std::strtod(str.data(), NULL);
+	if (a > DBL_MAX)
+		std::cout << "OVERFLOW" << std::endl;
+	else
+		std::cout << "ouaf: " << a << std::endl;
 	// if (*endp != 0)
 	// {
 	// 	std::cout << "overflow woo: " << a << std::endl;
