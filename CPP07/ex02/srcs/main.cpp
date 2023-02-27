dc79a3ff1157c74c40ae874ce2487f5848dfab75
Loic Johnson <ljohnson@z4r4p5.42lyon.fr>
2023-02-27 14:12:03 +0100

    CPP07 ex02: changed main to be a bit more readable, tests are done, push ongoing

--- CPP07/ex02/srcs/main.cpp
+++ CPP07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 10:39:44 by ljohnson          #+#    #+#             */
-/*   Updated: 2023/02/27 13:27:11 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2023/02/27 14:10:56 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 #define MAX_VAL 750
 int main(int, char**)
 {
+	std::cout << CYAN << BOLD;
+	std::cout << "numbers & mirror creation and array fill";
+	std::cout << FWHITE << std::endl;
+
 	Array<int> numbers(MAX_VAL);
 	int* mirror = new int[MAX_VAL];
 	srand(time(NULL));
@@ -25,50 +29,62 @@ int main(int, char**)
 		numbers[i] = value;
 		mirror[i] = value;
 	}
-	std::cout << "----------" << std::endl;
+
+//////////////////////////////////////////////////////////////////////
+
+	std::cout << std::endl;
+	std::cout << CYAN << BOLD;
+	std::cout << "scope double copy test";
+	std::cout << FWHITE << std::endl;
+
 	{
 		Array<int> tmp = numbers;
 		Array<int> test(tmp);
 	}
-	std::cout << "----------" << std::endl;
+
+//////////////////////////////////////////////////////////////////////
+
+	std::cout << std::endl;
+	std::cout << CYAN << BOLD;
+	std::cout << "Check if numbers and mirror got the same values";
+	std::cout << FWHITE << std::endl;
+
 	for (int i = 0; i < MAX_VAL; i++)
 	{
 		if (mirror[i] != numbers[i])
 		{
 			std::cerr << "didn't save the same value!!" << std::endl;
-			return 1;
+			return (1);
 		}
 	}
-	std::cout << "----------" << std::endl;
-	try
-	{
-		numbers[-2] = 0;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	std::cout << "----------" << std::endl;
-	try
-	{
-		numbers[MAX_VAL] = 0;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	std::cout << "----------" << std::endl;
+
+//////////////////////////////////////////////////////////////////////
+
+	std::cout << std::endl;
+	std::cout << CYAN << BOLD;
+	std::cout << "Out of Range exception tests -2 and MAX_VAL as index";
+	std::cout << FWHITE << std::endl;
+
+	try {numbers[-2] = 0;}
+	catch(const std::exception& e) {std::cerr << e.what() << '\n';}
+	try {numbers[MAX_VAL] = 0;}
+	catch(const std::exception& e) {std::cerr << e.what() << '\n';}
+	
+//////////////////////////////////////////////////////////////////////
+
+	std::cout << std::endl;
+	std::cout << CYAN << BOLD;
+	std::cout << "Change numbers array, mirror should still be different";
+	std::cout << FWHITE << std::endl;
+
 	for (int i = 0; i < MAX_VAL; i++)
-	{
 		numbers[i] = rand();
-	}
-	std::cout << "--------------------------------------------------" << std::endl;
-	numbers[2] = 123;
-	if (numbers[2] != mirror[2])
-		std::cout << "ouaf" << std::endl;
-	mirror[3] = 1234;
-	if (numbers[3] != mirror[3])
-		std::cout << "ouaf again" << std::endl;
+	if (numbers[0] != mirror[0])
+		std::cout << "numbers[0] & mirror[0] are different" << std::endl;
+	if (numbers[22] != mirror[22])
+		std::cout << "numbers[22] & mirror[22] are different" << std::endl;
+	if (numbers[300] != mirror[300])
+		std::cout << "numbers[300] & mirror[300] are different" << std::endl;
 	delete [] mirror;
-	return 0;
+	return (0);
 }
